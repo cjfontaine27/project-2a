2529137374109ffb18db5d5187af35961489032f
cjfontaine27 <cjfontaine11@gmail.com>
2024-12-07 19:37:52 -0500

    import

--- project-2a.js
+++ project-2a.js
@@ -5,6 +5,7 @@
 import { LitElement, html, css } from "lit";
 import { DDDSuper } from "@haxtheweb/d-d-d/d-d-d.js";
 import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
+import { WiredButton, WiredCheckbox, WiredCombo, WiredInput } from 'wired-elements';
 import "@haxtheweb/rpg-character/rpg-character.js";
 import "wired-elements";
 
